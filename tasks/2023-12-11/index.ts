0b58ba40815ca60f580a940b16ab8e16ab6a1629
Kacper Nadstoga <kapix007@gmail.com>
2023-12-11 23:42:10 +0100

    write function which checks if start or end city is included in graph

--- tasks/2023-12-11/index.ts
+++ tasks/2023-12-11/index.ts
@@ -4,5 +4,13 @@ export interface WeightedGraph {
   }
   
   export function findShortestPath(graph: WeightedGraph, startNode: string, endNode: string): string[] | null {
-    return []
-  }
+    // return []
+    if(!isCityOnMap(graph, startNode, endNode)) throw new Error('Invalid or non-existent route');
+    return [];
+}
+
+
+function isCityOnMap(grpah: WeightedGraph, startNode: string, endNode: string){
+    const cities = Object.keys(grpah);
+    return cities.includes(startNode) && cities.includes(endNode);
+}
