bc54e520c30deba9ab19423918ea29b58cf399bd
Kacper Nadstoga <kapix007@gmail.com>
2023-12-11 23:54:12 +0100

    prepare function to get data

--- tasks/2023-12-11/index.ts
+++ tasks/2023-12-11/index.ts
@@ -1,11 +1,23 @@
 // Tutaj skopiuj kod zadania
 export interface WeightedGraph {
     [key: string]: { [key: string]: number };
-  }
-  
-  export function findShortestPath(graph: WeightedGraph, startNode: string, endNode: string): string[] | null {
+}
+
+type AVAILABLE_CITIES = 'London' | 'Paris' | 'Tokyo' | 'Berlin' | 'Beijing' | 'Frankfurt' | "";
+
+type City = {
+    city: AVAILABLE_CITIES,
+    distanceToCity: number,
+    previousCity: AVAILABLE_CITIES
+}
+
+type QueueToCheck = City[];
+
+export function findShortestPath(graph: WeightedGraph, startNode: string, endNode: string): string[] | null {
     // return []
     if(!isCityOnMap(graph, startNode, endNode)) throw new Error('Invalid or non-existent route');
+    const rawCitiesData = queueToTraverse(graph);
+
     return [];
 }
 
@@ -13,4 +25,24 @@ export interface WeightedGraph {
 function isCityOnMap(grpah: WeightedGraph, startNode: string, endNode: string){
     const cities = Object.keys(grpah);
     return cities.includes(startNode) && cities.includes(endNode);
+}
+
+function dijkstra(graph: WeightedGraph, startNode: string): string[]{
+
+
+
+    return []
+}
+
+function queueToTraverse(graph: WeightedGraph): QueueToCheck {
+    const cities = Object.keys(graph);
+    const queueToTraverse: QueueToCheck = [];
+    cities.forEach(city => {
+        queueToTraverse.push({
+            city: city as AVAILABLE_CITIES,
+            distanceToCity: Infinity,
+            previousCity: ""
+        });
+    });
+    return queueToTraverse;
 }
