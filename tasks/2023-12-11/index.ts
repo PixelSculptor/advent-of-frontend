5a981ccb03a0d3d85f5bd749b1df93a1f74b63c6
Kacper Nadstoga <kapix007@gmail.com>
2023-12-11 23:07:10 +0100

    setup task 11-12

---
+++ tasks/2023-12-11/index.ts
@@ -0,0 +1,8 @@
+// Tutaj skopiuj kod zadania
+export interface WeightedGraph {
+    [key: string]: { [key: string]: number };
+  }
+  
+  export function findShortestPath(graph: WeightedGraph, startNode: string, endNode: string): string[] | null {
+    return []
+  }
