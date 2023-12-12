3d09d085a4c9721272da31a28afece0355368d0d
Kacper Nadstoga <kapix007@gmail.com>
2023-12-12 19:17:45 +0100

    resolve task-12-12

--- tasks/2023-12-12/index.ts
+++ tasks/2023-12-12/index.ts
@@ -4,6 +4,23 @@ export async function conductInterviews(
     interview: (subject: string) => Promise<string>,
     timeConstraint: number
   ): Promise<string[]> {
-    throw new Error('Not implemented');
-  }
-  
+    const plannedInterviews = subjects.map(async (subject) => {
+      const interviewInSchedule = restrictedPlanInterview(interview(subject).then(response => response).catch(err => err), timeConstraint);
+      try{
+        return await interviewInSchedule;
+      }catch(err){
+        return null;
+      }
+    });
+
+    return (await Promise.all(plannedInterviews)).filter(Boolean).map(String);
+}
+
+function restrictedPlanInterview(promiseInterview: Promise<string>, constraint: number) {
+  const timeoutInterview = new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error("Error: Subject is not allowed"));
+    }, constraint);
+  });
+  return Promise.race([promiseInterview, timeoutInterview]);
+}
