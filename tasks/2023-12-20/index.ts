ffe686058a89c1354f4921c960e88e1b3ccb49f8
Kacper Nadstoga <kapix007@gmail.com>
2023-12-20 22:24:01 +0100

    resolve task-20-12: problem with Gift Streaming machine

--- tasks/2023-12-20/index.ts
+++ tasks/2023-12-20/index.ts
@@ -1,4 +1,47 @@
 // Tutaj skopiuj kod zadania
-export class GiftStream {
-  
+
+interface IGiftStream {
+    stream: string[],
+    mappedGifts: string[];
+    map: (callback: (value: string) => string) => this;
+    skip: (offset: number) => this;
+    take: (count: number) => this;
+    getGifts: () => string[];
+}
+export class GiftStream implements IGiftStream{
+    stream: string[];
+    mappedGifts: string[];
+
+    constructor(_stream: string[]){
+        this.stream = _stream;
+        this.mappedGifts = [];
+    }
+
+    map(callback: (value: string) => string):this {
+        if(this.stream.length !== 0 ){
+            this.mappedGifts = this.stream.map(callback);
+        }
+        return this;
+    }
+
+    skip(offset: number):this {
+        if(this.stream.length <= offset){
+            this.mappedGifts = [];
+        }
+        this.mappedGifts = this.mappedGifts.slice(offset);
+    
+        return this;
+    };
+
+    take(count: number): this {
+        if(this.mappedGifts.length < count){
+            this.mappedGifts = [];
+        }
+        this.mappedGifts = this.mappedGifts.slice(0, count);
+        return this;
+    };
+
+    getGifts(): string[] {
+        return this.mappedGifts;
+    };
 }
