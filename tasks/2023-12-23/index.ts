f8dcc9f277d16b8aaa218e1ee3a9ab6d42966556
Kacper Nadstoga <kapix007@gmail.com>
2023-12-23 12:53:29 +0100

    setup temporary solution

--- tasks/2023-12-23/index.ts
+++ tasks/2023-12-23/index.ts
@@ -8,9 +8,40 @@ export type JsonSchema = {
   };
   
   export const generateSchema = (schemaDefinition: JsonSchema): JsonSchema => {
+    // const {type, required} = schemaDefinition
+    // const schema: JsonSchema = {type};
+    // if(schemaDefinition.type === 'object' && schemaDefinition.properties){
+
+    //     const { properties } = schemaDefinition;
+
+    //     const generateProperties: Record<string, JsonSchema> = {};
+    //     for(const [key, value] of Object.entries(properties)){
+    //         generateProperties[key] = generateSchema(value);
+    //     }
+    //     const schema: JsonSchema = {type};
+
+    //     schema.properties = {...generateProperties};
+
+
+    //     if(required?.length){
+    //         schema.required = required;
+    //     }
+    //     return schema;
+    // }else if(type === 'array'){
+    //     const {items, nullable} = schemaDefinition;
+    //     const schemaItems = items ? generateSchema(items) : {};
+    //     schema.items = {
+    //         type: 'array',
+    //         items: { type: typeof schemaItems[0]}
+    //     }
+    // }
     return schemaDefinition;
   };
   
   export const validate = (schema: JsonSchema, jsonObject: any): boolean => {
+    const requiredProperties = Object.keys(jsonObject);
+    if(!schema.required?.every(reqProp => requiredProperties.includes(reqProp))) return false;
+    
     return true;
-  };
+    
+};
